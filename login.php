bb6af3e104977a68a4155063608d38e810adc674
Nisa-Prodhan <43520501+Nisa-Prodhan@users.noreply.github.com>
2018-10-21 12:16:36 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,25 +1,23 @@
 <?php
-echo"Registration Successful";
-$user=$_POST['un'];
-$full=$_POST['fn'];
-$email=$_POST['ue'];
-$phone=$_POST['up'];
-$pass=$_POST['pw'];
-$cpass=$_POST['cpw'];
-$gen=$_POST['gen'];
-$edu=$_POST['edu'];
-
-echo "username :".$user;
-echo "<br>fullname :".$full;
-echo "<br>email :".$email;
-echo "<br>phone :".$phone;
-echo "<br>pass :".$pass;
-echo "<br>confirmpass :".$cpass;
-echo "<br>gen:".$gen;
-echo "<br>Education:";
-foreach($edu as $e)
+echo "<h1>Registration Successful</h1><br>";
+$user = $_POST['un'];//username
+$full = $_POST['fn'];//Fullname
+$email = $_POST['ue'];//Email Address
+$phone = $_POST['up'];//Phonr Number
+$pass = $_POST['pw'];//Password
+$gen = $_POST['gen'];//gender
+$edu = $_POST['edu'];//education 
+echo "Username : ".$user;
+echo "<br>Full Name : ".$full;
+echo "<br>Email : ".$email;
+echo "<br>Phone : ".$phone;
+echo "<br>Password : ".$pass;
+echo "<br>Gender : ".$gen;
+echo "<br>Education : ";
+echo "<ul>";
+foreach($edu as $ed)//print education array to a list
 {
-	echo $e."<br>";
+	echo "<li>".$ed."</li>";
 }
-
+echo "</ul>";
 ?>
